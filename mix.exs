abbc3efafa04711d0d29d0d1f27e8655c56003c6
Abby Malson <august.malson@gmail.com>
2026-09-02 09:56:19 +0300

    chore: Bump version to 0.1.83 to pick up runtime v0.14.66

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BotArmyFitness.MixProject do
   def project do
     [
       app: :bot_army_fitness,
-      version: "0.1.82",
+      version: "0.1.83",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
